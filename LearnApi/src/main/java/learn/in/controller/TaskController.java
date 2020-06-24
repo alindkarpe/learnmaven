3f4a00d0bcb562542b4d15a36009fad5e065eb69
gatij10 <Your-Email>
2020-06-24 13:43:54 +0530

    test commit

--- LearnApi/src/main/java/learn/in/controller/TaskController.java
+++ LearnApi/src/main/java/learn/in/controller/TaskController.java
@@ -22,7 +22,7 @@ public class TaskController {
 		
 		@RequestMapping(value="/getTasks",method = RequestMethod.GET)
 		public ResponseEntity<ResponseEntityBody> getTasks(){
-			logger.info("get all Tasks Today complete only");
+			logger.info("get all completed tasks");
 			return taskService.getTasks();
 		}
 }
